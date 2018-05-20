3cb952815d428a99c927fa521089e0c06878f152
F1nsk <malii14@student.sdu.dk>
2018-05-20 19:22:56 +0200

    minor changes

--- ludo/ludo_evo_player.cpp
+++ ludo/ludo_evo_player.cpp
@@ -16,24 +16,36 @@ ludo_evo_player::ludo_evo_player():
 }
 
 
-int ludo_evo_player::make_decision()
-{
-
-    for (int i = 0; i < 4; i++)
-    {
-        int currentPos = pos_start_of_turn[i];
-        int nextPos =  currentPos + dice_roll;
-
-
-
-
-
-
+int ludo_evo_player::make_decision(){
+    if(dice_roll == 6){
+        for(int i = 0; i < 4; ++i){
+            if(pos_start_of_turn[i]<0){
+                return i;
+            }
+        }
+        for(int i = 0; i < 4; ++i){
+            if(pos_start_of_turn[i]>=0 && pos_start_of_turn[i] != 99){
+                return i;
+            }
+        }
+    } else {
+        for(int i = 0; i < 4; ++i){
+            if(pos_start_of_turn[i]>=0 && pos_start_of_turn[i] != 99){
+                return i;
+            }
+        }
+        for(int i = 0; i < 4; ++i){ //maybe they are all locked in
+            if(pos_start_of_turn[i]<0){
+                return i;
+            }
+        }
     }
-    return nextPos;
-
-
+    return -1;
 }
+
+
+
+
 
 
 
@@ -52,6 +64,60 @@ void ludo_evo_player::start_turn(positions_and_dice relative)
 }
 
 
+void ludo_evo_player::post_game_analysis(std::vector<int> relative_pos)
+{
+    pos_end_of_turn = relative_pos;
+    bool game_complete = true;
+    for(int i = 0; i < 4; ++i){
+        if(pos_end_of_turn[i] < 99){
+            game_complete = false;
+        }
+    }
+    emit turn_complete(game_complete);
+}
+
+
+void ludo_evo_player::getPlayersPos() 
+{
+	
+}
+
+
+
+  
+bool ludo_evo_player::isEnemyBehind(currentPos)
+{
+}
+
+
+bool ludo_evo_player::isInGoal(currentPos) 
+{
+	
+}
+
+
+bool ludo_evo_player::canIdefend(currentPos)
+{
+	
+}
+
+bool ludo_evo_player::canIenterBoard()
+{
+	
+}
+
+bool ludo_evo_player::posibleToKill()
+{
+}
+
+bool ludo_evo_player::isPositionBlocked()
+{
+}
+ 
+vector<vector<bool>  ludo_evo_player::possibleMoveToMake()
+{
+}
+   
 
 
 
--- ludo/ludo_evo_player.h
+++ ludo/ludo_evo_player.h
@@ -12,6 +12,21 @@ class ludo_evo_player: public QObject
 
     Q_OBJECT
 private:
+	
+	struct possibleMoves 
+	{
+		int pieceNumber   // posible moves for the peice with this index 
+		// the following parameteres describe wether it is possible to certian moves, is set to 'true' the move is possible and if set to 'false' the move is not possible 
+		bool killFoe 	  //  
+		bool moveforward
+		bool moveToGlobe
+		bool EnemyIsClose 
+		bool defend 
+		bool 
+		
+		
+	}
+	
 
     std::vector<int> pos_start_of_turn;
     std::vector<int> pos_end_of_turn;
@@ -24,29 +39,35 @@ private:
     const int goal = 99;
     const int GOAL = 56;
     const int home = -1;
+    vector<int> MyPiecesPos;                   //container for my pieces
+    vector<int> EnemyPiecesPos;                   //container for enemy pieces
+	
     
     // ************************************************************************************************
     // functions  to explore the board 
     // ************************************************************************************************
-    bool isEnemyBehind(currentPos);  // check is enemy player is within striking range 
-    bool isInGoal(currentPos); // is play in goal stretch 
-    bool canIdefend(currentPos);  //
-    bool canIenterBoard(); //
-    bool posibleToKill(); 
-    bool isPositionBlocked(); // check if two players are already on the spot 
-   
+    bool isEnemyBehind(int peiceNumber);  // check is enemy player is within striking range 
+    bool isInGoal(int peiceNumber); // is play in goal stretch 
+    bool canIdefend(int peiceNumber);  //
+    bool canIenterBoard(int peiceNumber); //
+    bool posibleToKill(int peiceNumber); 
+    bool isPositionBlocked(int peiceNumber); // check if two players are already on the spot 
+    void getPlayersPos(); 
+    bool moveToFinish(int peiceNumber); 
+    bool moveInFinish(int peiceNumber);
+    bool leaveHomePossible(int peiceNumber);
    
    
 	// ************************************************************************************************
 	// actions 
 	// ************************************************************************************************
-	void killEnemy(); 
-	void normalMove(); 
-	void defendFriend();
-	void MoveOutofhome(); 
-	void moveToStar(); 
-	void moveToGlobe();  
-	void moveTosafeZone(); 
+	//~ void killEnemy(); 
+	//~ void normalMove(); 
+	//~ void defendFriend();
+	//~ void MoveOutofhome(); 
+	//~ void moveToStar(); 
+	//~ void moveToGlobe();  
+	//~ void moveTosafeZone(); 
 	
 	
 	
